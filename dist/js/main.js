e24a3e50642fa0d21a2c0ed6edeca1ca49a0e4ba
arichild <arieevq@gmail.com>
2023-10-10 02:01:56 +0300

    adaptive index.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,3 @@
-const slider = document.querySelector('.main-screen-range')
-const letters = document.querySelectorAll('.main-screen-letter')
-const backgroundBlock = document.querySelector('.main-screen-block')
-const background = document.querySelector('.main-screen-bg')
-const sliderTxt = document.querySelector('.main-screen-txt')
-
 $(document).ready(function() {
   // animation for development.html
   AOS.init({
@@ -11,13 +5,26 @@ $(document).ready(function() {
     duration: 1200,
   });
 
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
   // slider
   const newsSliderEl = document.querySelector('.splide.newsSlider')
+  const moreSliderEl = document.querySelector('.splide.moreSlider')
   const achievementsEl = document.querySelector('.splide.achievementsSlider')
   const galleryEl = document.querySelector('.splide.gallerySlider')
   const experienceSlider = document.querySelector('.splide.experienceSlider')
 
-  if(newsSliderEl !== null) {
+  if(newsSliderEl) {
     const slider = new Splide('.splide.newsSlider', {
       perPage: 3,
       pagination: false,
@@ -25,14 +32,36 @@ $(document).ready(function() {
 
       breakpoints: {
         1024: { perPage: 2, gap: 30 },
-        768 : { perPage: 1, gap: 0 },
+        599 : { perPage: 1, gap: 0 },
       },
     })
 
     slider.mount()
   }
 
-  if(achievementsEl !== null) {
+  if(moreSliderEl) {
+    const slider = new Splide('.splide.moreSlider', {
+      perPage: 3,
+      pagination: false,
+      gap: 30,
+
+      breakpoints: {
+        1024: { perPage: 2, gap: 30 },
+        599 : { perPage: 1, gap: 0 },
+      },
+    })
+
+    let items = document.querySelectorAll('.splide__slide');
+    let controls = document.querySelector('.splide__arrows');
+
+    if(items.length <= 3) {
+      controls.classList.add('hidden')
+    }
+
+    slider.mount()
+  }
+
+  if(achievementsEl) {
     const slider = new Splide('.splide.achievementsSlider', {
       perPage: 4,
       pagination: false,
@@ -41,24 +70,31 @@ $(document).ready(function() {
       breakpoints: {
         1024: { perPage: 3 },
         768 : { perPage: 2 },
-        576: {perPage: 1}
+        599: {perPage: 1}
       },
     })
 
     slider.mount()
   }
 
-  if(galleryEl !== null) {
+  if(galleryEl) {
     const slider = new Splide('.splide.gallerySlider', {
       perPage: 3,
       pagination: true,
       gap: 30
     })
 
+    let items = galleryEl.querySelectorAll('.splide__slide');
+    let controls = galleryEl.querySelector('.splide-controls');
+
+    if(items.length <= 3) {
+      controls.classList.add('hidden')
+    }
+
     slider.mount()
   }
 
-  if(experienceSlider !== null) {
+  if(experienceSlider) {
     const slider = new Splide('.splide.experienceSlider', {
       perPage: 3,
       gap: 30,
@@ -66,7 +102,7 @@ $(document).ready(function() {
 
       breakpoints: {
         1024: { perPage: 2 },
-        768 : { perPage: 1, gap: 0 },
+        599 : { perPage: 1, gap: 0 },
       },
     })
 
@@ -77,20 +113,9 @@ $(document).ready(function() {
   const tabs = document.querySelector('.ui-tab') || document.querySelector('.ui-tab-icon');
 
   if(tabs) {
-    const content = document.querySelectorAll('.ui-tabcontent');
-
     tabs.addEventListener('click', (e) => {
       const parent = e.target.closest('.ui-tab-block')
-      const currTab = e.target.dataset.tab;
-      const tab = e.target;
-      const tabContent = content[currTab - 1]
 
-      if (!currTab) {
-        return;
-      }
-
-      tab.classList.toggle('active')
-      tabContent.classList.toggle('active')
       parent.classList.toggle('active')
     })
   }
@@ -98,45 +123,58 @@ $(document).ready(function() {
   // anchor
   const anchors = document.querySelectorAll('[data-anchor]')
 
-  if(anchors.length) {
-    for (let anchor of anchors) {
-      anchor.addEventListener('click', function (e) {
-        e.preventDefault()
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
 
-        let id = e.target.dataset.anchor;
+      let id = e.target.dataset.anchor;
 
-        if(id) {
-          document.getElementById(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'center',
-          })
+      if(id && document.getElementById(id)) {
+        let offset = (document.getElementById(id).getBoundingClientRect().top + window.pageYOffset) - document.querySelector('.header').clientHeight
+
+        if(document.getElementById(id).dataset.aos) {
+          offset = offset - 100
         }
-      })
-    }
+
+        window.scrollTo({
+          behavior: 'smooth',
+          top: offset
+        })
+      }
+    })
   }
 
   // input file
-  const inputFile = document.querySelector('.ui-doc-file input[type=file]')
+  const inputFile = $('.input-file')
 
   if(inputFile) {
-    $('.ui-doc-file input[type=file]').on('change', function(){
-      let file = this.files[0];
-      $(this).next().html(file.name);
+    inputFile.each(function() {
+      $(this).on('change', function(){
+        let file = this.files[0];
+        $(this).next().html(file.name);
+      });
     });
   }
 
   // lang menu
   const langBtn = document.querySelector('.header-lang-select')
-  const closeLangMenu = document.querySelector('.header-lang-default')
-  const langMenu = document.querySelector('.header-lang-menu')
 
-  langBtn.addEventListener(('click'), () => {
-    langMenu.classList.add('active')
-  })
+  if(langBtn) {
+    const closeLangMenu = document.querySelector('.header-lang-default')
+    const langMenu = document.querySelector('.header-lang-menu')
 
-  closeLangMenu.addEventListener(('click'), () => {
-    langMenu.classList.remove('active')
-  })
+    langBtn.addEventListener('click', () => {
+      if(!langMenu) return
+
+      langMenu.classList.add('active')
+    })
+
+    closeLangMenu.addEventListener('click', () => {
+      if(!langMenu) return
+
+      langMenu.classList.remove('active')
+    })
+  }
 
   // form styler select
   $(function() {
@@ -151,10 +189,14 @@ $(document).ready(function() {
     burger.addEventListener('click', () => {
       const menuBurger = document.querySelector('.header-content-block')
       const header = document.querySelector('.header')
+      const logo = document.querySelector('.header-content-img')
+      const body = document.body
 
       burger.classList.toggle('active')
       menuBurger.classList.toggle('active')
       header.classList.toggle('active')
+      logo.classList.toggle('active')
+      body.classList.toggle('active')
     })
   }
 
@@ -203,13 +245,57 @@ $(document).ready(function() {
     Thumbs: false
   });
 
+  let header = document.querySelector(".header");
+
+  if(header) {
+    window.addEventListener('scroll', headerSticky)
+  }
+
   // height for blocks for development.html
-  document.addEventListener("DOMContentLoaded", setHeightDevelopment)
+  setHeightDevelopment()
   window.addEventListener('resize', setHeightDevelopment);
 
   // range input
+  const slider = document.querySelector('.main-screen-range')
+
   if(slider) {
+    const letters = document.querySelectorAll('.main-screen-letter')
+    const backgroundBlock = document.querySelector('.main-screen-block')
+    const background = document.querySelector('.main-screen-bg')
+    const sliderTxt = document.querySelector('.main-screen-txt')
+
+    mainScreen()
     slider.addEventListener('input', mainScreen)
+
+    function mainScreen() {
+      let sliderPos = slider.value / (100 / window.innerWidth);
+
+      if(window.innerWidth > 1024) {
+        if(slider.value < 2) {
+          background.style.opacity = 0;
+          sliderTxt.style.opacity = 0;
+
+          return
+        } else if(slider.value > 99) {
+          background.style.opacity = 1;
+          sliderTxt.style.opacity = 1;
+          backgroundBlock.style.width ='100%';
+
+          return
+        } else {
+          sliderTxt.style.opacity = 1;
+        }
+
+        for (let i = 0; i < letters.length; i++) {
+          let letterPos = letters[i].getBoundingClientRect().left;
+
+          letters[i].style.opacity = letterPos <= sliderPos ? 1 : (slider.value / 100);
+        }
+
+        backgroundBlock.style.width = sliderPos + 'px';
+        background.style.opacity = (slider.value / 100);
+      }
+    }
   }
 });
 
@@ -235,40 +321,42 @@ $('select.custom-select').on('change', function() {
   }, 1)
 });
 
-if(document.getElementById('phone')) {
-  let phone = document.getElementById('phone')
+let phone = document.querySelectorAll('.phone-mask')
 
-  let phoneMask = IMask(phone, {
-    mask: [
-      {
-        mask: '+{375} (00) 000 00 00',
-        startsWith: '375',
-        overwrite: true,
-        lazy: false,
-        placeholderChar: '_',
-      },
-      {
-        mask: '+{7} (000) 000 00 00',
-        startsWith: '7',
-        overwrite: true,
-        lazy: false,
-        placeholderChar: '_',
-      },
-      {
-        mask: '+0000000000000',
-        startsWith: '',
-        country: 'unknown'
+if(phone.length) {
+  phone.forEach(element => {
+    IMask(element, {
+      mask: [
+        {
+          mask: '+{375} (00) 000 00 00',
+          startsWith: '375',
+          overwrite: true,
+          lazy: false,
+          placeholderChar: '_',
+        },
+        {
+          mask: '+{7} (000) 000 00 00',
+          startsWith: '7',
+          overwrite: true,
+          lazy: false,
+          placeholderChar: '_',
+        },
+        {
+          mask: '+0000000000000',
+          startsWith: '',
+          country: 'unknown'
+        }
+      ],
+
+      dispatch: function (appended, dynamicMasked) {
+        var number = (dynamicMasked.value + appended).replace(/\D/g, '');
+
+        return dynamicMasked.compiledMasks.find(function (m) {
+          return number.indexOf(m.startsWith) === 0;
+        });
       }
-    ],
-
-    dispatch: function (appended, dynamicMasked) {
-      var number = (dynamicMasked.value + appended).replace(/\D/g, '');
-
-      return dynamicMasked.compiledMasks.find(function (m) {
-        return number.indexOf(m.startsWith) === 0;
-      });
-    }
-  })
+    })
+  });
 }
 
 function showPopup(path) {
@@ -295,70 +383,56 @@ function showPopup(path) {
 function setHeightDevelopment() {
   const blockParent = document.querySelectorAll('.development-block')
 
-  if(blockParent.length) {
-    blockParent.forEach(item => {
-      const blockInfo = item.querySelector('.development-block-info')
+  if(!blockParent.length) return
 
-      const parentHeight = item.offsetHeight
-      const blockInfoBottomHeight = item.querySelector('.development-block-bottom').offsetHeight
-      const title = item.querySelector('.development-block-title')
-      const titleHeight = item.querySelector('.development-block-title').offsetHeight
-      const imgBlock = item.querySelector('.development-block-img')
-      const imgBlockHeight = imgBlock.offsetHeight
+  blockParent.forEach(item => {
+    const blockInfo = item.querySelector('.development-block-info')
 
-      const paddingTop = +window.getComputedStyle(item).paddingTop.split('px')[0]
-      const paddingBottom = +window.getComputedStyle(item).paddingBottom.split('px')[0]
+    const parentHeight = item.offsetHeight
+    const blockInfoBottomHeight = item.querySelector('.development-block-bottom').offsetHeight
+    const title = item.querySelector('.development-block-title')
+    const titleHeight = item.querySelector('.development-block-title').offsetHeight
+    const imgBlock = item.querySelector('.development-block-img')
+    const imgBlockHeight = imgBlock.offsetHeight
 
-      const marginBottom = +window.getComputedStyle(blockInfo).marginBottom.split('px')[0]
-      const marginTop = +window.getComputedStyle(blockInfo).marginTop.split('px')[0]
+    const paddingTop = +window.getComputedStyle(item).paddingTop.split('px')[0]
+    const paddingBottom = +window.getComputedStyle(item).paddingBottom.split('px')[0]
 
-      const titleMarginBottom = +window.getComputedStyle(title).marginBottom.split('px')[0]
+    const marginBottom = +window.getComputedStyle(blockInfo).marginBottom.split('px')[0]
+    const marginTop = +window.getComputedStyle(blockInfo).marginTop.split('px')[0]
 
-      const imgBlockMarginBottom = +window.getComputedStyle(imgBlock).marginBottom.split('px')[0]
+    const titleMarginBottom = +window.getComputedStyle(title).marginBottom.split('px')[0]
 
-      const sumPadding = paddingTop + paddingBottom
-      const sumMargin = marginBottom + marginTop
+    const imgBlockMarginBottom = +window.getComputedStyle(imgBlock).marginBottom.split('px')[0]
 
-      let sum = parentHeight - (titleMarginBottom + titleHeight + sumPadding + sumMargin + blockInfoBottomHeight)
+    const sumPadding = paddingTop + paddingBottom
+    const sumMargin = marginBottom + marginTop
+
+    let sum = parentHeight - (titleMarginBottom + titleHeight + sumPadding + sumMargin + blockInfoBottomHeight)
+    blockInfo.style.height = sum  + 'px'
+
+    if(window.innerWidth <= 1024) {
+      sum = sum - imgBlockHeight - imgBlockMarginBottom
+
       blockInfo.style.height = sum  + 'px'
-
-      if(window.innerWidth <= 1024) {
-        sum = sum - imgBlockHeight - imgBlockMarginBottom
-
-        blockInfo.style.height = sum  + 'px'
-      }
-    });
-  }
+    }
+  });
 }
 
-document.addEventListener("DOMContentLoaded", mainScreen)
+// header sticky
+function headerSticky() {
+  let logo = header.querySelector('.header-content-img')
+  let cardBreackpoint = document.querySelector(".card-breackpoint");
 
-function mainScreen() {
-  if(slider) {
-    let sliderPos = slider.value / (100 / window.innerWidth);
+  if (cardBreackpoint) {
+    let offset = cardBreackpoint.offsetTop - header.clientHeight;
 
-    if(slider.value < 2) {
-      background.style.opacity = 0;
-      sliderTxt.style.opacity = 0;
-
-      return
-    } else if(slider.value > 99) {
-      background.style.opacity = 1;
-      sliderTxt.style.opacity = 1;
-      backgroundBlock.style.width ='100%';
-
-      return
+    if (window.pageYOffset >= offset) {
+      header.classList.add("sticky");
+      logo.classList.add("visible");
     } else {
-      sliderTxt.style.opacity = 1;
+      header.classList.remove("sticky");
+      logo.classList.remove("visible");
     }
-
-    for (let i = 0; i < letters.length; i++) {
-      let letterPos = letters[i].getBoundingClientRect().left;
-
-      letters[i].style.opacity = letterPos <= sliderPos ? 1 : (slider.value / 100);
-    }
-
-    backgroundBlock.style.width = sliderPos + 'px';
-    background.style.opacity = (slider.value / 100);
   }
 }
